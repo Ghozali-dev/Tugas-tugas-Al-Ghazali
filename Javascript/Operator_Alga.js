ca8b4c2b7c0fa33c97503ebbbeaa55fe238ee65e
Ghozali-dev <ghozaliimpo@gmail.com>
2026-08-20 21:27:07 +0800

    Tugas1

---
+++ Javascript/Operator_Alga.js
@@ -0,0 +1,156 @@
+// ===== Assignment =====
+
+let variabel = 90;
+let variabel2 = 40;
+
+
+
+
+
+// ===== ARITHMETIC =====
+
+// Operator Jumlah (+)
+var a = 30;
+var b = 15;
+var jumlah = a + b;
+
+console.log(jumlah); // Jumlah = 45
+
+// Operator Pengurangan (-)
+var c = 30;
+var d = 15;
+var selisih = c - d;
+
+console.log(selisih); // Selisih = 15
+
+// Operator Perkalian (*)
+var e = 30;
+var f = 15;
+var hasilKali = e * f;
+
+console.log(hasilKali); // Hasil Kali = 450
+
+// Operator Pembagian (/)
+var g = 30;
+var h = 15;
+var hasilBagi = g / h;
+
+console.log(hasilBagi); // Hasil Bagi = 2
+
+// Operator Sisa Bagi (%)
+var i = 30;
+var j = 15;
+var sisaBagi = i % j;
+
+console.log(sisaBagi); // Sisa Bagi = 0
+
+// Operator Kurung ()
+var k = 30;
+var l = 15;
+var hasilKurung = (k + l) * 2;
+
+console.log(hasilKurung); // Hasil Kurung = 90
+
+
+
+
+
+// ===== COMPARISON =====
+
+// Operator Sama Dengan (==)
+var pw = "iqis_12345";
+var pw2 = "iqis_12345";
+var hasil = pw == pw2;
+console.log(hasil); // hasil = true
+
+// Operator Tidak Sama Dengan (!=)
+var pw3 = "izin_doeloe";
+var pw4 = "izin_dulu";
+var hasil2 = pw3 != pw4;
+console.log(hasil2); // hasil = true
+
+// Operator Lebih Besar (>)
+let umur = 25;
+let umur2 = 20;
+let lebihBesar = umur > umur2;
+console.log(lebihBesar); // Lebih Besar = true
+
+// Operator Lebih Kecil (<)
+var tinggi = 170;
+var tinggi2 = 180;
+var lebihKecil = tinggi < tinggi2;
+console.log(lebihKecil); // Lebih Kecil = false
+
+// Operator Lebih Besar atau Sama Dengan (>=)
+var nilai = 80;
+let nilai2 = 75;
+let output = nilai >= nilai2;
+console.log(output); // Lebih Besar atau Sama Dengan = true
+
+// Operator Lebih Kecil atau Sama Dengan (<=)
+var nilai3 = 70;
+var nilai4 = 75;
+var output2 = nilai3 <= nilai4;
+console.log(output2); // Lebih Kecil atau Sama Dengan = true
+
+// Operator Identik (===)
+var angka = 10;
+var angka2 = "10";
+var identik = angka === angka2;
+console.log(identik); // Identik = false
+
+// Operator Tidak Identik (!==)
+var number1 = 10;
+var number2 = "10";
+var isN1NotEqualN2 = number1 !== number2;
+console.log(isN1NotEqualN2); // Tidak Identik = true
+
+
+
+
+
+// ===== LOGICAL =====
+var x = 10;
+var y = 20;
+// Operator AND (&&)
+var hasilAnd = (x > 5) && (y < 25);
+console.log(hasilAnd); // Hasil AND = true
+
+// Operator OR (||)
+var hasilOr = (x < 5) || (y < 25);
+console.log(hasilOr); // Hasil OR = true
+
+// Operator NOT (!)
+var hasilNot = !(x > 5);
+console.log(hasilNot); // Hasil NOT = false
+
+
+
+
+
+// ===== String + =====
+var str1 = "Hello";
+var str2 = "World";
+var hasilString = str1 + " " + str2;
+console.log(hasilString); // Hasil String = Hello World
+
+
+
+
+
+// ===== Increment & Decrement =====
+// Increment (++)
+var a = 10;
+console.log(a++);
+console.log(a++);
+console.log(a++);
+console.log(a++);
+console.log(a++);
+
+// Decrement (--)
+var b = 20;
+console.log(b--);
+console.log(b--);
+console.log(b--);
+console.log(b--);
+console.log(b--);
